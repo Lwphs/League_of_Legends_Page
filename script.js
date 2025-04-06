a7a8ae920a3ceff64e399e92b1760bf9c6294afe
HectorGarciaCarlota <arnau.garcia@gracia.lasalle.cat>
2025-04-06 17:22:58 +0200

    feat(js): function

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const modalContent = document.querySelector(".modalContent");
 const message = document.querySelector("#cookieRejectMessage");
 const acceptBtn = document.querySelector("#acceptCookies");
 const rejectBtn = document.querySelector("#rejectCookies");
+
+acceptBtn.addEventListener("click", function () {
+    cookieModal.style.display = "none"; 
+});
+
+rejectBtn.addEventListener("click", function () {
+    cookieModal.style.display = "none"; 
+    message.style.display = "flex"; 
+    
+});
