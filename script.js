50d4d5bf653f6301d331ba723b83c380aeb50a67
HectorGarciaCarlota <arnau.garcia@gracia.lasalle.cat>
2025-04-06 17:22:33 +0200

    feat(js): creating variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const cookieModal = document.querySelector(".cookieModal");
+const modalContent = document.querySelector(".modalContent");
+const message = document.querySelector("#cookieRejectMessage");
+const acceptBtn = document.querySelector("#acceptCookies");
+const rejectBtn = document.querySelector("#rejectCookies");
