6ef366bb35824de52bf47fb592d60eaa1ac307b5
ArnauGarcia <arnau.garcia@gracia.lasalle.cat>
2025-04-08 18:32:54 +0200

    feat(js): a lot of things only for a closeBtn

--- scripts/signIn.js
+++ scripts/signIn.js
@@ -6,3 +6,18 @@ signInBtn.addEventListener("click", function () {
     loginSection.classList.add("show"); 
     screenOpacity.style.display = "flex"; 
 });
+
+const closeBtn = document.getElementById("closeBtn");
+
+closeBtn.addEventListener("click", function () {
+    loginSection.classList.remove("show"); 
+    screenOpacity.style.display = "none"; 
+});
+
+closeBtn.addEventListener("mouseenter", function () {
+    closeBtn.src = "assets/closeBtn_hover.svg"
+});
+
+closeBtn.addEventListener("mouseleave", function () {
+    closeBtn.src = "assets/closeBtn.svg"
+});
