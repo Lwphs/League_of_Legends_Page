cd7581c9a36246da3090430617c0b0848712d9d1
ArnauGarcia <arnau.garcia@gracia.lasalle.cat>
2025-04-10 01:07:00 +0200

    item1 function

--- scripts/modeSwap.js
+++ scripts/modeSwap.js
@@ -6,3 +6,41 @@ const videoSource = bgVideo.querySelector("source");
 const aram = "assets/videos/aramDemo.mp4";
 const rift = "assets/videos/riftDemo.mp4";
 const arena = "assets/videos/arenaDemo.mp4";
+
+item2.addEventListener("click", function () {
+    const img1 = item1.querySelector("img");
+    img1.style.filter = "grayscale(100%)";
+    img1.style.transform = "scale(1)";
+
+    const img2 = item2.querySelector("img");
+    img2.style.filter = "grayscale(0)";
+    img2.style.transform = "scale(1.4)";
+
+    const img3 = item3.querySelector("img");
+    img3.style.filter = "grayscale(100%)";
+    img3.style.transform = "scale(1)";
+
+    videoSource.src = rift;
+    bgVideo.load();
+    bgVideo.play();
+});
+
+item3.addEventListener("click", function () {
+    const img1 = item1.querySelector("img");
+    img1.style.filter = "grayscale(100%)";
+    img1.style.transform = "scale(1)";
+
+    const img2 = item2.querySelector("img");
+    img2.style.filter = "grayscale(100%)";
+    img2.style.transform = "scale(1)";
+
+    const img3 = item3.querySelector("img");
+    img3.style.filter = "grayscale(0)";
+    img3.style.transform = "scale(1.4)";
+
+    videoSource.src = arena;
+    bgVideo.load();
+    bgVideo.play();
+});
+
+
