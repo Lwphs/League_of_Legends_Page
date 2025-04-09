71c73b5768f0c5852688191be1393746307e97d1
ArnauGarcia <arnau.garcia@gracia.lasalle.cat>
2025-04-10 01:07:21 +0200

    functions modeSwap.js

--- scripts/modeSwap.js
+++ scripts/modeSwap.js
@@ -1,11 +1,33 @@
 const item1 = document.getElementById("item1");
 const item2 = document.getElementById("item2");
 const item3 = document.getElementById("item3");
+
 const bgVideo = document.getElementById("bgvideo");
 const videoSource = bgVideo.querySelector("source");
+
 const aram = "assets/videos/aramDemo.mp4";
 const rift = "assets/videos/riftDemo.mp4";
 const arena = "assets/videos/arenaDemo.mp4";
+
+const modes = document.getElementById("modes");
+
+item1.addEventListener("click", function () {
+    const img1 = item1.querySelector("img");
+    img1.style.filter = "grayscale(0)";
+    img1.style.transform = "scale(1.4)";
+
+    const img2 = item2.querySelector("img");
+    img2.style.filter = "grayscale(100%)";
+    img2.style.transform = "scale(1)";
+
+    const img3 = item3.querySelector("img");
+    img3.style.filter = "grayscale(100%)";
+    img3.style.transform = "scale(1)";
+
+    videoSource.src = aram;
+    bgVideo.load();
+    bgVideo.play();
+});
 
 item2.addEventListener("click", function () {
     const img1 = item1.querySelector("img");
